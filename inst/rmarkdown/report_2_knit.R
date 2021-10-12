94a1a1890bce8bd784ebe1f61c4c512fe80b2eca
David Hughes <hughes.evoanth@gmail.com>
2021-10-12 18:05:07 +0100

    update htm report file template

--- inst/rmarkdown/report_2_knit.R
+++ inst/rmarkdown/report_2_knit.R
@@ -13,14 +13,14 @@ knitr::opts_chunk$set(warning=FALSE, echo=TRUE, message=FALSE,
 
 ## Load the Rdata file
 
-```{r}
-load("GeneratedData.Rdata")
+```{r load_Rdata}
+# load("GeneratedData.Rdata")
 ```
 ---
 
 ## A picture of daisy built into package
 
-```{r , out.width = "200px"}
+```{r daisy_pic, out.width = "200px"}
 f = system.file("rmarkdown", package="exrender")
 pic = file.path( f, "skeleton/daisy.png")
 knitr::include_graphics(pic)
@@ -31,7 +31,7 @@ knitr::include_graphics(pic)
 
 We made this image in the make_some_data() function and passed it to the report as a paramater that needed to be loaded.
 
-```{r, fig.width = 8, fig.height = 6}
+```{r ggplot_fig, fig.width = 8, fig.height = 6}
 my_plot
 ```
 
@@ -39,7 +39,7 @@ my_plot
 
 A table of the first few rows of the embedded data set 'ng_anno'
 
-```{r}
+```{r knit_table}
 knitr::kable(ng_anno[1:4,])
 ```
 
